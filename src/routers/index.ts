9f098149404fd960806212045ab2ae99a91039ce
cambo <cambo@bure.com.au>
2022-11-17 21:04:24 +1100

    add root route

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth";
-import authenticationMiddleware from "../oauth/authenticate";
 
 const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("This server has no content.");
+});
 
 router.use("/auth", authRouter);
 
